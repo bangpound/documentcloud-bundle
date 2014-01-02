f6d30b0f5c741f81c120b67c12baf0d4049f9a9f
Benjamin Doherty <bjd@bangpound.org>
2014-01-02 17:08:43 -0500

    remove env vars.

--- BinaryDriver/DocsplitDriver.php
+++ BinaryDriver/DocsplitDriver.php
@@ -27,11 +27,6 @@ class DocsplitDriver extends AbstractBinary
         }
 
         $process = $this->factory->create($command);
-        $process->setEnv([
-            'GEM_PATH' => '/Users/bjd/.rvm/gems/ruby-1.9.3-p448@afsc:/Users/bjd/.rvm/gems/ruby-1.9.3-p448@global',
-            'GEM_HOME' => '/Users/bjd/.rvm/gems/ruby-1.9.3-p448@afsc',
-            'PATH' => '/Users/bjd/.rvm/gems/ruby-1.9.3-p448@afsc/bin:/Users/bjd/.rvm/gems/ruby-1.9.3-p448@global/bin:/Users/bjd/.rvm/rubies/ruby-1.9.3-p448/bin:/Users/bjd/.rvm/bin:/usr/local/pear/bin:/usr/local/share/npm/bin:/Developer/usr/bin:/Users/bjd/.composer/vendor/bin:/Users/bjd/bin:/Users/bjd/bin/sandbox_tools:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/MacGPG2/bin:/Users/bjd/.rvm/bin',
-        ]);
 
         return $this->run($process, $bypassErrors, $listeners);
     }
